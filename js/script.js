30478100491625bf17a3eff124d331b341f0d066
TerellAdams <s_terell.adams@ousd.org>
2020-01-23 02:28:40 +0000

    Added objects,vars,and other thing project almost completed

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
-let playList = ["Hot girl bummer", "Do Re Mi", "Roxxane"];
+//let playList = ["Hot girl bummer", "Do Re Mi", "Roxxane"];
 
-$('.button').click(function() {
+let song = {
+  music: "Hot girl bummer",
+  artist: "Black bear",
+  length: "2:48",
+}
+let song2 = {
+  music: "Do Re Mi",
+  artist: "Black bear",
+  length: "2:58",
+}
+let song3 = {
+  music: "Roxxane",
+  artist: "Arizonna",
+  length: "3:00",
+}
+$('#song').click(function () {
+  $("#song").append("<p> music : " + info["music"] + "</p>");
+  $("#song").append("<p> artist : " + info["artist"] + "</p>");
+  $("#song").append("<p> length : " + info["length"] + "</p>");
 
-   playlist.forEach(function(music) {
-  $('.playList').append(text);
-  });
+  playlist.forEach(function (music) {
+    $('.playList').append(text);
+  })
 });
